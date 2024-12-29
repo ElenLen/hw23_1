09430f46f297c2f9b32be2d6f4649d0900933e1c
ElenLen <relax-pl@yandex.ru>
2024-12-29 17:29:03 +0500

    init: start project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+// import TariffList from './components/TariffList';
+import './App.css';
+import Tariff from './components/Tariff.jsx';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Интернет тарифы</h1>
+      <Tariff />
+    </div>
+  );
+}
+
+export default App;
